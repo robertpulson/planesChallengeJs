18f235d75eb13d995872313a5a289da5f614fc09
Phil Brockwell <nineteen40eight@gmail.com>
2015-03-04 10:38:29 +0000

    all tests passing

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,5 +1,4 @@
 describe('Airport', function() {
-  var airport
   var plane;
 
   beforeEach(function() {
@@ -7,18 +6,52 @@ describe('Airport', function() {
     plane = jasmine.createSpyObj('plane', ['land', 'takeOff', 'isFlying']);
   });
 
-  describe('', function() {
+  it('can land a plane', function() {
+    airport.confirmLand(plane);
+    expect(airport.planes).toEqual([plane]);
+    expect(plane.land).toHaveBeenCalledWith();
+  });
 
-    it('can land a plane', function() {
+  it('can dispatch a plane', function() {
+    airport.confirmLand(plane);
+    expect(airport.planes).toEqual([plane]);
+    airport.dispatch(plane);
+    expect(airport.planes).toEqual([]);
+    expect(plane.takeOff).toHaveBeenCalledWith();
+  });
+
+  it('knows how many planes it contains', function() {
+    airport.confirmLand(plane);
+    expect(airport.planes.length).toEqual(1);
+  });
+
+  it('knows when it is full', function() {
+    for (var i = 6; i > 0; i--) {
       airport.confirmLand(plane);
-      expect(airport.planes).toEqual([plane]);
-    });
+    };
+    expect(airport.isFull()).toBe(true);
+  });
 
-    it('knows how many planes it contains', function() {
+  it('does not allow a plane to land if it is full', function() {
+    for (var i = 6; i > 0; i--) {
       airport.confirmLand(plane);
-      expect(airport.planes.length).toEqual(1);
-    });
+    };
+    expect(function(){ airport.confirmLand(plane); }).toThrow(new Error('The airport is full come back later!'));
+  });
 
+  it('knows if the weather is Ok', function() {
+    spyOn(airport, 'isWeatherOk').and.returnValue(true);
+    expect(airport.isWeatherOk()).toBe(true);
+  });
+
+  it('knows if the weather is not Ok', function() {
+    spyOn(airport, 'isWeatherOk').and.returnValue(false);
+    expect(airport.isWeatherOk()).toBe(false);
+  });
+
+  it('does not allow a plane to land if the weather is not ok', function() {
+    spyOn(airport, 'isWeatherOk').and.returnValue(false);
+    expect(function(){ airport.confirmLand(plane); }).toThrow(new Error('The weather is not ok try again later!'));
   });
 
 });
--- src/airport.js
+++ src/airport.js
@@ -1,8 +1,28 @@
 Airport = function() {
   this.planes = [];
+  this.capacity = 6;
 };
 
-Airport.prototype.confirmLand = function (plane) {
+Airport.prototype.confirmLand = function(plane) {
+  if (this.isFull() === true) {
+    throw new Error('The airport is full come back later!');
+  } else if (this.isWeatherOk() === false) {
+    throw new Error('The weather is not ok try again later!');
+  } else {
   this.planes.push(plane);
-  plane.land;
+  plane.land();
+  };
+};
+
+Airport.prototype.dispatch = function(plane) {
+  this.planes.splice(this.planes.indexOf(plane));
+  plane.takeOff();
+};
+
+Airport.prototype.isFull = function() {
+  if (this.planes.length >= this.capacity) return true;
+};
+
+Airport.prototype.isWeatherOk = function() {
+  if (Math.floor((Math.random() * 5) + 1) != 1) return true;
 };
